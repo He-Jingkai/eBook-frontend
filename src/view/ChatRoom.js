e133f8b053815f53ef84b585e116e0d042ca05f7
He-Jingkai <He-JingKai@outlook.com>
2021-09-22 21:30:33 +0800

    init

---
+++ src/view/ChatRoom.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import Chat, { Bubble, Card, CardText } from '@chatui/core';
+import '@chatui/core/es/styles/index.less';
+import '@chatui/core/dist/index.css';
+
+var websocket;
+
+export class ChatRoom extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            messages:[],
+            username:"xxx",
+        }
+    }
+
+    send =  (message, callback)=> {
+        this.waitForConnection(function () {
+            websocket.send(message);
+            if (typeof callback !== 'undefined') {
+                callback();
+            }
+        }, 1000);
+    };
+
+    waitForConnection=(callback, interval)=>{
+        if (websocket.readyState === 1) {
+            callback();
+        } else {
+            let that = this;
+            setTimeout(function () {
+                that.waitForConnection(callback, interval);
+            }, interval);
+        }
+    };
+
+
+    componentWillMount() {
+        fetch("http://localhost:8080/user?userid="+localStorage.getItem("id"))
+            .then(response => response.json())
+            .then(data => {
+                // alert("data:" + data);
+                this.setState({
+                    username:data.username
+                });
+                websocket=new WebSocket("ws://localhost:8080/chatRoom")
+                websocket.onmessage = this.onMessage;
+
+
+                let joinMsg = {
+                    type:"join",
+                    username:data.username,
+                };
+                this.send(JSON.stringify(joinMsg));
+            }).catch(function (ex) {
+            console.log('parsing failed', ex)
+        })
+    }
+
+    onMessage=(evt)=> {
+        var msg = JSON.parse(evt.data);
+        let messageNow;
+        if(msg.type==='text')
+            messageNow={
+                type: 'text',
+                content: msg.content,
+                username: msg.username,
+            }
+        else if(msg.type==='join')
+            messageNow={
+                type: 'join',
+                username: msg.username,
+            }
+        else
+            messageNow={
+                type: 'leave',
+                username: msg.username,
+            }
+        let messagePrevious=this.state.messages
+        messagePrevious.push(messageNow)
+        this.setState({messages:messagePrevious})
+    }
+
+
+    handleSend=(type, val)=> {
+        if (type === 'text' && val.trim()) {
+            let messagePrevious=this.state.messages
+            let messageNew={
+                type: 'text',
+                content: val,
+                username:this.state.username,
+                position: 'right',
+            }
+            let messageSend={
+                type: 'text',
+                content: val,
+                username:this.state.username,
+            }
+
+            messagePrevious.push(messageNew)
+            this.setState({messages:messagePrevious})
+            // TODO: 发送请求
+            websocket.send(JSON.stringify(messageSend));
+        }
+    }
+
+    renderMessageContent=(msg)=> {
+        switch (msg.type) {
+            case 'text':
+                return <Bubble color={"#178FFE"} type={'text'}>
+                    <div style={{
+                        textAlign:"left",
+                    }}>
+                        <p>
+                            {msg.content}
+                        </p>
+                        <div
+                            style={{
+                                color:"#888",
+                                fontSize:"9px"
+                            }}
+                        >{msg.username}</div>
+                    </div>
+                </Bubble>;
+            case 'join':
+                return (
+                    <Card size="xl">
+                        <CardText>{msg.username+"加入了聊天室"}</CardText>
+                    </Card>
+                );
+            case 'leave':
+                return (
+                    <Card size="xl">
+                        <CardText>{msg.username+"离开了聊天室"}</CardText>
+                    </Card>
+                );
+            default:
+                return null;
+        }
+    }
+    render() {
+        return (
+            <Chat
+                navbar={{ title: '聊天室' }}
+                messages={this.state.messages}
+                renderMessageContent={this.renderMessageContent}
+                onSend={this.handleSend}
+            />
+        );
+    }
+}
